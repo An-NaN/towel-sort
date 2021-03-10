7ca1fe49b18805f64ab268f5ff6d61ce861f99a6
An-NaN <an-na.ilyuhina@yandex.ru>
2021-03-10 23:29:10 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+	if (matrix === undefined || matrix.length === 0) { return [] }
+	else {
+		for (let i = 1; i < matrix.length; ) {
+			let item = matrix[i].reverse();
+			matrix.splice(i, 1, item);
+			i += 2;
+		}
+	}
+	return matrix.reduce((sum, item) => sum.concat(item), []);
 }
